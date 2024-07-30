4d0cb3b91d8259838a19e7bcb6639b3b879657c5
Justin Klitgaard <jlke45@gmial.com>
2024-07-30 11:44:47 +0900

    Configure Tailwind and DaisyUI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.{ejs}"],
+  content: ["./views/*.ejs", "./views/partials/*.ejs"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: ["cyberpunk"],
   },
   plugins: [require("daisyui")],
 };
